66b783067ef86c891d685705b075f812cf8ecbd4
PrDM99 <neodire89@gmail/com>
2023-03-27 03:04:18 -0700

    We rewrote the function printf with c,s,% specifiers

---
+++ main.h
@@ -0,0 +1,28 @@
+#ifndef MAIN_H
+#define MAIN_H
+#include <stdarg.h>
+#include <unistd.h>
+#include <stdlib.h>
+
+/**
+ * struct printer - A new struct type defining a printer.
+ * @symbol: A symbol representing a data type.
+ * @print: A function pointer to a function that prints
+ *         a data type corresponding to symbol.
+ */
+
+typedef struct printer
+{
+	char *symbol;
+	int (*print)(va_list args);
+
+} prints;
+
+int _putchar(char c);
+int _printf(const char *format, ...);
+int print_char(va_list args);
+int print_int(va_list args);
+int print_string(va_list args);
+int print_rev_string(va_list args);
+
+#endif
