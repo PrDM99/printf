5cd22fc8b60e09b7765a741a7d1639b995a86004
PrDM99 <neodire89@gmail/com>
2023-03-28 04:21:22 -0700

    Edits

--- main.h
+++ main.h
@@ -22,7 +22,13 @@ int _putchar(char c);
 int _printf(const char *format, ...);
 int print_char(va_list args);
 int print_int(va_list args);
+int print_unsigned_int(va_list args);
+int print_dec_binary(va_list args);
 int print_string(va_list args);
 int print_rev_string(va_list args);
+int print_rot13_string(va_list args);
+int print_hex(va_list args);
+int print_HEX(va_list args);
+int print_octal(va_list args);
 
 #endif
