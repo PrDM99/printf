f33fab5e35c866f92f46d73045eb4d9cc694272c
Ngulolu <b.ngulolu@gmail.com>
2023-03-28 02:31:58 -0700

    More edits

--- _printf.c
+++ _printf.c
@@ -1,82 +1,38 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
- * _printf - produces output according to a format
- * @format: a character string composed of zero or more directives
- *
- * Return: length of output
+ * _printf - Receives the main string and all the necessary parameters to
+ * print a formated string
+ * @format: A string containing all the desired characters
+ * Return: A total count of the characters printed
  */
 
 int _printf(const char *format, ...)
 {
-	va_list args;
 
-	prints specifier[] = {
-		{"c", print_char},
-		{"i", print_int},
+va_list = args;
+
+prints specifiers [] = {
 		{"d", print_int},
+		{"i", print_int},
+		{"c", print_char},
 		{"s", print_string},
+		{"b", print_binary},
+		{"u", print_unsigned_integer},
+		{"o", print_octal},
+		{"x", print_hex},
 		{"r", print_rev_string},
-		{'\0', NULL}
+		{"R", print_rot13},
+		{NULL, NULL},
 	};
-	int i = 0, j = 0;
-	int len_spec;
-	int size = 0;
-
-	va_start(args, format);
-
-	len_spec = sizeof(specifier) / sizeof(specifier[0]);
-	len_spec -= 1;
+	va_list args;
 
 	if (format == NULL)
 		return (-1);
 
-	while (*(format + i) != '\0')
-	{
-		j = 0;
-
-		if (*(format + i) == '%')
-		{
-			i += 1;
-
-			while (j < len_spec &&
-			       format[i] != (specifier[j].symbol[0]))
-					j++;
-
-			if (*(format + i) == '\0')
-				return (-1);
-			else if (*(format + i) == '%')
-			{
-				_putchar('%');
-				size += 1;
-				i += 1;
-			}
-			else if (j < len_spec)
-			{
-				size += specifier[j].print(args);
-
-				if (size == -1)
-					return (-1);
-				i += 1;
-			}
-			else
-			{
-				_putchar('%');
-				_putchar(*(format + i));
-				size += 1;
-				i += 1;
-			}
-		}
-		else
-		{
-			_putchar(*(format + i));
-			size += 1;
-			i += 1;
-		}
-	}
-
+	va_start(args, format);
+	size_spec = sizeof(specifier) / sizeof(specifier[0]);
+	size_spec -= 1;
 	va_end(args);
-
-	return (size);
+	return (size_spec);
 }
--- print_int.c
+++ print_int.c
@@ -9,31 +9,28 @@
 
 int print_int(va_list args)
 {
-    int i = 0;
-    int j = 0;
-    int count = 1;
-    int size = 0;
-    int n = va_arg(args, int);
+int i = 0;
+int j = 0;
+int count = 1;
+int size = 0;
+int n = va_arg(args, int);
 
-    if (n < 0)
-    {
-        _putchar('-');
-        i = n * -1;
-        size++;
-    }
-    else
-        i = n;
-
-    j = i;
-
-    for (; j > 9; j /= 10, count *= 10) {}
-
-    for (; count >= 1; count /= 10)
-    {
-        _putchar(((i / count) % 10) + 48);
-        size++;
-    }
-
-    return size;
+if (n < 0)
+{
+_putchar('-');
+i = n * -1;
+size++;
 }
+else
+i = n;
 
+j = i;
+
+for (; j > 9; j /= 10, count *= 10)
+for (; count >= 1; count /= 10)
+{
+_putchar(((i / count) % 10) + 48);
+size++;
+}
+return (size);
+}
